5d1380345f8d43be6bbc960d864d783b210a5ad6
Kirk Hendrick <hendrick.kirk@gmail.com>
2016-11-01 21:34:53 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp'),
+    mocha = require('gulp-mocha'),
+    watch = require('gulp-watch');
+
+gulp.task('monitorTests', function () {
+    watchTests();
+});
+
+gulp.task('test', function () {
+    return gulp.src('../test/tests.js', {read: false})
+        .pipe(mocha({reporter: 'nyan'}))
+        .pipe(gulp.dest('build'));
+});
+
+function watchTests() {
+    return watch(['../*.js', '../**/*.js'], function () {
+        gulp.src('../test/tests.js', {read: false})
+            .pipe(mocha({reporter: 'nyan'}))
+            .pipe(gulp.dest('build'));
+    });
+}
---
+++ test/test.js
@@ -0,0 +1,5 @@
+const assert = require('assert');
+
+describe('Utility Functions: ', function () {
+});
+
